705565f4f8aaff4403a6f56ddfe15e9b02d1418f
johnston <johnstonvs@gmail.com>
2023-05-22 13:36:31 -0500

    Completes challenge

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
+import cookieSession from 'cookie-session';
+
+const server = express();
+const port = 3000;
+server.use(cookieParser());
+server.use(cookieSession({
+  name: 'user_session',
+  secret: 'supersecretstring'
+}))
+
+server.get('/', (req, res) => {
+  res.send('Go to login to set name');
+})
+
+server.get('/login', (req, res) => {
+  let name = 'Jacob';
+  res.cookie('name', name).send(`Name set to ${name}`);
+})
+
+server.get('/hello', (req, res) => {
+  console.log(req.cookies)
+  let name = req.cookies.name;
+  res.send(`Hello ${name}`);
+})
+
+server.listen(port, () => {
+  console.log(`server running on port ${port}`);
+})
